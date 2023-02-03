531272de05513e35fc940620deed6d5e6d7ee2ab
winstxnhdw <winstxnhdw@gmail.com>
2023-02-03 20:04:30 +0800

    feat: add pylint and remove pandas

--- cubic_spline_interpolator.py
+++ cubic_spline_interpolator.py
@@ -1,84 +1,137 @@
 import numpy as np
-
-from numpy.typing import ArrayLike
+from numpy.typing import ArrayLike, NDArray
 from scipy.interpolate import CubicSpline
 
-def initialise_cubic_spline(x: ArrayLike, y: ArrayLike, ds: float, bc_type: str) -> tuple[CubicSpline, np.ndarray]:
 
-    distance = np.concatenate((np.zeros(1), np.cumsum(np.hypot(np.ediff1d(x), np.ediff1d(y)))))
+def initialise_cubic_spline(x: ArrayLike, y: ArrayLike, distance_step: float, boundary_condition_type: str) -> tuple[CubicSpline, NDArray]:
+    """
+    Summary
+    -------
+    Initialises the cubic spline interpolator
+
+    Parameters
+    ----------
+    x (ArrayLike): x coordinates of the waypoints
+    y (ArrayLike): y coordinates of the waypoints
+    distance_step (float): distance step between the waypoints
+    boundary_condition_type (str): boundary condition type
+
+    Returns
+    -------
+    cubic_spline (CubicSpline): cubic spline interpolator
+    s (NDArray): distance coordinate of the cubic spline path
+    """
+    distances = np.concatenate((np.zeros(1), np.cumsum(np.hypot(np.ediff1d(x), np.ediff1d(y)))))
     points = np.array([x, y]).T
-    s = np.arange(0, distance[-1], ds)
-    
+    s = np.arange(0, distances[-1], distance_step)
+
     try:
-        cs = CubicSpline(distance, points, bc_type=bc_type, axis=0, extrapolate=False)
-        
-    except ValueError as e:
-        raise ValueError(f"{e} If you are getting a sequence error, do check if your input dataset contains consecutive duplicate(s).")
- 
-    return cs, s
+        cubic_spline = CubicSpline(distances, points, bc_type=boundary_condition_type, axis=0, extrapolate=False)
 
-def generate_cubic_spline(x: ArrayLike, y: ArrayLike, ds: float=0.05, bc_type: str='natural') -> tuple[np.ndarray, ...]:
-    
-    cs, s = initialise_cubic_spline(x, y, ds, bc_type)
+    except ValueError:
+        print("If you are getting a sequence error, do check if your input dataset contains consecutive duplicate(s).")
+        raise
 
-    dx, dy = cs.derivative(1)(s).T
+    return cubic_spline, s
+
+
+def generate_cubic_spline(x: ArrayLike, y: ArrayLike, distance_step: float=0.05, boundary_condition_type: str='natural') -> tuple[NDArray, ...]:
+    """
+    Summary
+    -------
+    Generates a cubic spline path with its yaw and curvature
+
+    Parameters
+    ----------
+    x (ArrayLike): x coordinates of the waypoints
+    y (ArrayLike): y coordinates of the waypoints
+    distance_step (float): distance step between the waypoints
+    boundary_condition_type (str): boundary condition type
+
+    Returns
+    -------
+    spline_x (NDArray): x coordinates of the cubic spline path
+    spline_y (NDArray): y coordinates of the cubic spline path
+    yaw (NDArray): yaw of the cubic spline path
+    curvature (ndarray): curvature of the cubic spline path
+    """
+    cubic_spline, s = initialise_cubic_spline(x, y, distance_step, boundary_condition_type)
+
+    dx, dy = cubic_spline.derivative(1)(s).T
     yaw = np.arctan2(dy, dx)
 
-    ddx, ddy = cs.derivative(2)(s).T
+    ddx, ddy = cubic_spline.derivative(2)(s).T
     curvature = (ddy*dx - ddx*dy) / ((dx*dx + dy*dy)**1.5)
 
-    cx, cy = cs(s).T
-    return cx, cy, yaw, curvature
+    spline_x, spline_y = cubic_spline(s).T
+    return spline_x, spline_y, yaw, curvature
 
-def generate_cubic_path(x: ArrayLike, y: ArrayLike, ds: float=0.05, bc_type: str='natural') -> tuple[np.ndarray, np.ndarray]:
 
-    cs, s = initialise_cubic_spline(x, y, ds, bc_type)
-    cx, cy = cs(s).T
-    return cx, cy
-    
-def calculate_spline_yaw(x: ArrayLike, y: ArrayLike, ds: float=0.05, bc_type: str='natural') -> np.ndarray:
-    
-    cs, s = initialise_cubic_spline(x, y, ds, bc_type)
-    dx, dy = cs.derivative(1)(s).T
+def generate_cubic_path(x: ArrayLike, y: ArrayLike, distance_step: float=0.05, boundary_condition_type: str='natural') -> tuple[NDArray, NDArray]:
+    """
+    Summary
+    -------
+    Generates a cubic spline path
+
+    Parameters
+    ----------
+    x (ArrayLike): x coordinates of the waypoints
+    y (ArrayLike): y coordinates of the waypoints
+    distance_step (float): distance step between the waypoints
+    boundary_condition_type (str): boundary condition type
+
+    Returns
+    -------
+    spline_x (ndarray): x coordinates of the cubic spline path
+    spline_y (ndarray): y coordinates of the cubic spline path
+    """
+    cubic_spline, s = initialise_cubic_spline(x, y, distance_step, boundary_condition_type)
+    spline_x, spline_y = cubic_spline(s).T
+
+    return spline_x, spline_y
+
+
+def calculate_spline_yaw(x: ArrayLike, y: ArrayLike, distance_step: float=0.05, boundary_condition_type: str='natural') -> NDArray:
+    """
+    Summary
+    -------
+    Calculates the yaw of a cubic spline path without generating the path
+
+    Parameters
+    ----------
+    x (ArrayLike): x coordinates of the waypoints
+    y (ArrayLike): y coordinates of the waypoints
+    distance_step (float): distance step between the waypoints
+    boundary_condition_type (str): boundary condition type
+
+    Returns
+    -------
+    yaw (NDArray): yaw of the cubic spline path
+    """
+    cubic_spline, s = initialise_cubic_spline(x, y, distance_step, boundary_condition_type)
+    dx, dy = cubic_spline.derivative(1)(s).T
+
     return np.arctan2(dy, dx)
 
-def calculate_spline_curvature(x: ArrayLike, y: ArrayLike, ds: float=0.05, bc_type: str='natural') -> np.ndarray:
 
-    cs, s = initialise_cubic_spline(x, y, ds, bc_type)
-    dx, dy = cs.derivative(1)(s).T
-    ddx, ddy = cs.derivative(2)(s).T
+def calculate_spline_curvature(x: ArrayLike, y: ArrayLike, distance_step: float=0.05, boundary_condition_type: str='natural') -> NDArray:
+    """
+    Summary
+    -------
+    Calculates the curvature of a cubic spline path without generating the path
+
+    Parameters
+    ----------
+    x (ArrayLike): x coordinates of the waypoints
+    y (ArrayLike): y coordinates of the waypoints
+
+    Returns
+    -------
+    curvature (NDArray): curvature of the cubic spline path
+    """
+    cubic_spline, s = initialise_cubic_spline(x, y, distance_step, boundary_condition_type)
+    first_derivative = cubic_spline.derivative(1)
+    dx, dy = first_derivative(s).T
+    ddx, ddy = first_derivative.derivative(1)(s).T
+
     return (ddy*dx - ddx*dy) / ((dx*dx + dy*dy)**1.5)
-
-def main():
-    
-    import pandas as pd
-    from matplotlib import pyplot as plt
-
-    dir_path = 'tests/waypoints.csv'
-    df = pd.read_csv(dir_path)
-    x = df['x'].values
-    y = df['y'].values
-
-    px, py = generate_cubic_path(x, y)
-    pyaw = calculate_spline_yaw(x, y)
-    pk = calculate_spline_curvature(x, y)
-
-    fig, ax = plt.subplots(1, 3, figsize=(15, 5))
-    plt.style.use('seaborn-pastel')
-
-    ax[0].set_box_aspect(1)
-    ax[0].set_title('Geometry')
-    ax[0].plot(px, py, c='m')
-
-    ax[1].set_box_aspect(1)
-    ax[1].set_title('Yaw')
-    ax[1].plot(pyaw, c='m')
-
-    ax[2].set_box_aspect(1)
-    ax[2].set_title('Curvature')
-    ax[2].plot(pk, c='m')
-    
-    plt.show()
-
-if __name__ == '__main__':
-    main()
