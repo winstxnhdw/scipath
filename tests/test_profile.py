22c34fe730907ba5801742ecf42afe6e85e44661
winstxnhdw <winstxnhdw@gmail.com>
2024-10-27 14:26:24 +0000

    tests: use `sum` instead of `mean`

--- tests/test_profile.py
+++ tests/test_profile.py
@@ -3,13 +3,6 @@
 from scipath import Profile, create_cubic_path_2d
 
 
-def test_profile_all(waypoints: list[tuple[float, float]]):
-    cubic_path = create_cubic_path_2d(waypoints, profile=Profile.ALL)
-    assert cubic_path.path is not None
-    assert cubic_path.yaw is not None
-    assert cubic_path.curvature is not None
-
-
 def test_profile_path(waypoints: list[tuple[float, float]]):
     cubic_path = create_cubic_path_2d(waypoints, profile=Profile.PATH)
     assert cubic_path.path is not None
@@ -20,7 +13,7 @@ def test_profile_path(waypoints: list[tuple[float, float]]):
 def test_profile_yaw(waypoints: list[tuple[float, float]]):
     cubic_path = create_cubic_path_2d(waypoints, profile=Profile.YAW)
     assert cubic_path.path is None
-    assert cubic_path.yaw is not None
+    assert cubic_path.yaw.sum() == 17424.47461197635
     assert cubic_path.curvature is None
 
 
@@ -28,13 +21,13 @@ def test_profile_curvature(waypoints: list[tuple[float, float]]):
     cubic_path = create_cubic_path_2d(waypoints, profile=Profile.CURVATURE)
     assert cubic_path.path is None
     assert cubic_path.yaw is None
-    assert cubic_path.curvature is not None
+    assert cubic_path.curvature.sum() == 38.22376728677102
 
 
 def test_profile_no_curvature(waypoints: list[tuple[float, float]]):
     cubic_path = create_cubic_path_2d(waypoints, profile=Profile.NO_CURVATURE)
     assert cubic_path.path is not None
-    assert cubic_path.yaw is not None
+    assert cubic_path.yaw.sum() == 17424.47461197635
     assert cubic_path.curvature is None
 
 
@@ -42,11 +35,11 @@ def test_profile_no_yaw(waypoints: list[tuple[float, float]]):
     cubic_path = create_cubic_path_2d(waypoints, profile=Profile.NO_YAW)
     assert cubic_path.path is not None
     assert cubic_path.yaw is None
-    assert cubic_path.curvature is not None
+    assert cubic_path.curvature.sum() == 38.22376728677102
 
 
 def test_profile_no_path(waypoints: list[tuple[float, float]]):
     cubic_path = create_cubic_path_2d(waypoints, profile=Profile.NO_PATH)
     assert cubic_path.path is None
-    assert cubic_path.yaw is not None
-    assert cubic_path.curvature is not None
+    assert cubic_path.yaw.sum() == 17424.47461197635
+    assert cubic_path.curvature.sum() == 38.22376728677102
--- tests/test_regression.py
+++ tests/test_regression.py
@@ -5,5 +5,5 @@ from scipath import Profile, create_cubic_path_2d
 
 def test_regression(waypoints: list[tuple[float, float]]) -> None:
     cubic_path = create_cubic_path_2d(waypoints, profile=Profile.ALL)
-    assert cubic_path.yaw.mean() == 1.3113926854802702
-    assert cubic_path.curvature.mean() == 0.0028767793547656373
+    assert cubic_path.yaw.sum() == 17424.47461197635
+    assert cubic_path.curvature.sum() == 38.22376728677102
