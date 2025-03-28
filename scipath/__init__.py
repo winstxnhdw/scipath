b8306700ceecff6c4e8877349452ffbebcf63e0c
winstxnhdw <winston.ho@redbulltechnology.com>
2025-03-28 03:32:27 +0000

    feat: expose types

--- scipath/__init__.py
+++ scipath/__init__.py
@@ -1,2 +1,6 @@
+from scipath.cubic_path2d import ConsecutiveDuplicateError as ConsecutiveDuplicateError
+from scipath.cubic_path2d import CubicPath2D as CubicPath2D
+from scipath.cubic_path2d import FloatArray as FloatArray
+from scipath.cubic_path2d import Points as Points
 from scipath.cubic_path2d import Profile as Profile
 from scipath.cubic_path2d import create_cubic_path_2d as create_cubic_path_2d
