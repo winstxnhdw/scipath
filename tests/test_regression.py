8aee9eeed41fa9063e1b5c63f31385a5613de5a0
winstxnhdw <winstxnhdw@gmail.com>
2025-06-22 03:38:08 +0100

    test: use `isclose` for float comparison

--- tests/test_regression.py
+++ tests/test_regression.py
@@ -1,9 +1,11 @@
-# ruff: noqa: S101, PLR2004
+# ruff: noqa: S101
+
+from math import isclose
 
 from scipath import Profile, create_cubic_path_2d
 
 
 def test_regression(waypoints: list[tuple[float, float]]) -> None:
     cubic_path = create_cubic_path_2d(waypoints, profile=Profile.ALL)
-    assert float(cubic_path["yaw"].sum()) == 17424.47461197635
-    assert float(cubic_path["curvature"].sum()) == 38.22376728677102
+    assert isclose(cubic_path["yaw"].sum(), 17424.47461197635)
+    assert isclose(cubic_path["curvature"].sum(), 38.22376728677102)
