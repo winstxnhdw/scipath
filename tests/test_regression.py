aa17c5ecb1fcd4104d49580bcb7370978d94ea86
winstxnhdw <winstxnhdw@gmail.com>
2025-06-22 03:05:59 +0100

    tests: convert NumPy float to Python float

--- tests/test_regression.py
+++ tests/test_regression.py
@@ -5,5 +5,5 @@ from scipath import Profile, create_cubic_path_2d
 
 def test_regression(waypoints: list[tuple[float, float]]) -> None:
     cubic_path = create_cubic_path_2d(waypoints, profile=Profile.ALL)
-    assert cubic_path["yaw"].sum() == 17424.47461197635
-    assert cubic_path["curvature"].sum() == 38.22376728677102
+    assert float(cubic_path["yaw"].sum()) == 17424.47461197635
+    assert float(cubic_path["curvature"].sum()) == 38.22376728677102
